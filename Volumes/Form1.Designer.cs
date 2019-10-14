193a737f3cfa36501914fe8bb6a42fa3ca2fb6d6
Abdullah Ayub <rc0039895@student.riversidecollege.ac.uk>
2019-10-14 15:16:38 +0100

    Add project files.

---
+++ Volumes/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Volumes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tP1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.L1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.L2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.L3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.BtnC1 = new System.Windows.Forms.Button();
+            this.BtnC2 = new System.Windows.Forms.Button();
+            this.LP1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.LP2 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.LP3 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.LT = new System.Windows.Forms.Label();
+            this.LT1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tP1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tP1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(827, 555);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tP1
+            // 
+            this.tP1.Controls.Add(this.LT1);
+            this.tP1.Controls.Add(this.BtnC1);
+            this.tP1.Controls.Add(this.textBox3);
+            this.tP1.Controls.Add(this.L3);
+            this.tP1.Controls.Add(this.textBox2);
+            this.tP1.Controls.Add(this.L2);
+            this.tP1.Controls.Add(this.textBox1);
+            this.tP1.Controls.Add(this.L1);
+            this.tP1.Controls.Add(this.pictureBox1);
+            this.tP1.Location = new System.Drawing.Point(4, 22);
+            this.tP1.Name = "tP1";
+            this.tP1.Padding = new System.Windows.Forms.Padding(3);
+            this.tP1.Size = new System.Drawing.Size(819, 529);
+            this.tP1.TabIndex = 0;
+            this.tP1.Text = "Series";
+            this.tP1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.LT);
+            this.tabPage2.Controls.Add(this.textBox6);
+            this.tabPage2.Controls.Add(this.LP3);
+            this.tabPage2.Controls.Add(this.textBox5);
+            this.tabPage2.Controls.Add(this.LP2);
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Controls.Add(this.LP1);
+            this.tabPage2.Controls.Add(this.BtnC2);
+            this.tabPage2.Controls.Add(this.pictureBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(819, 529);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Parallel";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Volumes.Properties.Resources.iwabwne;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(813, 530);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Volumes.Properties.Resources.weufhnewfhnewfe123;
+            this.pictureBox2.Location = new System.Drawing.Point(-4, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(823, 533);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // L1
+            // 
+            this.L1.AutoSize = true;
+            this.L1.Location = new System.Drawing.Point(339, 187);
+            this.L1.Name = "L1";
+            this.L1.Size = new System.Drawing.Size(33, 13);
+            this.L1.TabIndex = 1;
+            this.L1.Text = "R1 = ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(378, 184);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // L2
+            // 
+            this.L2.AutoSize = true;
+            this.L2.Location = new System.Drawing.Point(478, 286);
+            this.L2.Name = "L2";
+            this.L2.Size = new System.Drawing.Size(33, 13);
+            this.L2.TabIndex = 3;
+            this.L2.Text = "R2 = ";
+            this.L2.Click += new System.EventHandler(this.L2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(517, 283);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // L3
+            // 
+            this.L3.AutoSize = true;
+            this.L3.Location = new System.Drawing.Point(339, 401);
+            this.L3.Name = "L3";
+            this.L3.Size = new System.Drawing.Size(33, 13);
+            this.L3.TabIndex = 5;
+            this.L3.Text = "R3 = ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(378, 398);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // BtnC1
+            // 
+            this.BtnC1.Location = new System.Drawing.Point(565, 42);
+            this.BtnC1.Name = "BtnC1";
+            this.BtnC1.Size = new System.Drawing.Size(208, 59);
+            this.BtnC1.TabIndex = 7;
+            this.BtnC1.Text = "Calculate";
+            this.BtnC1.UseVisualStyleBackColor = true;
+            // 
+            // BtnC2
+            // 
+            this.BtnC2.Location = new System.Drawing.Point(571, 34);
+            this.BtnC2.Name = "BtnC2";
+            this.BtnC2.Size = new System.Drawing.Size(198, 71);
+            this.BtnC2.TabIndex = 1;
+            this.BtnC2.Text = "Calculate";
+            this.BtnC2.UseVisualStyleBackColor = true;
+            // 
+            // LP1
+            // 
+            this.LP1.AutoSize = true;
+            this.LP1.Location = new System.Drawing.Point(250, 214);
+            this.LP1.Name = "LP1";
+            this.LP1.Size = new System.Drawing.Size(30, 13);
+            this.LP1.TabIndex = 2;
+            this.LP1.Text = "R1 =";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(286, 211);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.TextBox4_TextChanged);
+            // 
+            // LP2
+            // 
+            this.LP2.AutoSize = true;
+            this.LP2.Location = new System.Drawing.Point(424, 214);
+            this.LP2.Name = "LP2";
+            this.LP2.Size = new System.Drawing.Size(30, 13);
+            this.LP2.TabIndex = 4;
+            this.LP2.Text = "R2 =";
+            this.LP2.Click += new System.EventHandler(this.LP2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(460, 211);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // LP3
+            // 
+            this.LP3.AutoSize = true;
+            this.LP3.Location = new System.Drawing.Point(618, 214);
+            this.LP3.Name = "LP3";
+            this.LP3.Size = new System.Drawing.Size(30, 13);
+            this.LP3.TabIndex = 6;
+            this.LP3.Text = "R3 =";
+            this.LP3.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(654, 211);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 7;
+            // 
+            // LT
+            // 
+            this.LT.AutoSize = true;
+            this.LT.Location = new System.Drawing.Point(226, 60);
+            this.LT.Name = "LT";
+            this.LT.Size = new System.Drawing.Size(54, 13);
+            this.LT.TabIndex = 8;
+            this.LT.Text = "R Total = ";
+            // 
+            // LT1
+            // 
+            this.LT1.AutoSize = true;
+            this.LT1.Location = new System.Drawing.Point(227, 66);
+            this.LT1.Name = "LT1";
+            this.LT1.Size = new System.Drawing.Size(51, 13);
+            this.LT1.TabIndex = 8;
+            this.LT1.Text = "R Total =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 590);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tP1.ResumeLayout(false);
+            this.tP1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tP1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label L2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label L1;
+        private System.Windows.Forms.Button BtnC1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label L3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label LP1;
+        private System.Windows.Forms.Button BtnC2;
+        private System.Windows.Forms.Label LP3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label LP2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label LT1;
+        private System.Windows.Forms.Label LT;
+    }
+}
+
